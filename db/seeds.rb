ff9964936b93b2f68d986c5f0c4aed7199568456
Rishav Rastogi <rishav.rastogi@gmail.com>
2017-07-13 22:36:49 -0700

    Added Seed Content for Node

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+['A', 'B', 'C'].each do |name|
+  Node.create(name: name)
+end
